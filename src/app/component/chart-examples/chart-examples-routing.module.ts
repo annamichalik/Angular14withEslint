7e87b64e6bc76c3a1330fbeac2cf8cfbcc905818
Anna Michalik <anna.michalik@bluecrestinc.com>
2023-12-08 15:56:57 +0100

    added line chart : https://valor-software.com/ng2-charts/#LineChart

--- src/app/component/chart-examples/chart-examples-routing.module.ts
+++ src/app/component/chart-examples/chart-examples-routing.module.ts
@@ -2,6 +2,7 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {BarChartComponent} from "./bar-chart/bar-chart.component";
 import {PieChartComponent} from "./pie-chart/pie-chart.component";
+import {LineChartComponent} from "./line-chart/line-chart.component";
 
 const routes: Routes = [
   {
@@ -11,7 +12,12 @@ const routes: Routes = [
   {
     path: 'pie',
     component: PieChartComponent
+  },
+  {
+    path: 'line',
+    component: LineChartComponent
   }
+
 ];
 
 @NgModule({
--- src/app/component/chart-examples/chart-examples.module.ts
+++ src/app/component/chart-examples/chart-examples.module.ts
@@ -6,12 +6,14 @@ import {BarChartComponent} from './bar-chart/bar-chart.component';
 import {NgChartsModule} from "ng2-charts";
 import {PieChartComponent} from './pie-chart/pie-chart.component';
 import {MatDividerModule} from "@angular/material/divider";
+import { LineChartComponent } from './line-chart/line-chart.component';
 
 
 @NgModule({
   declarations: [
     BarChartComponent,
-    PieChartComponent
+    PieChartComponent,
+    LineChartComponent
   ],
   imports: [
     CommonModule,
